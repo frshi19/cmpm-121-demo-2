45e187016702abcab133d373fc2fa6bd0840def6
Frank Shi <frshi@ucsc.edu>
2024-10-14 20:30:30 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ const APP_NAME = "Frank's Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const appTitle: HTMLHeadingElement = document.createElement("h1");
+appTitle.innerHTML = APP_NAME;
+app.appendChild(appTitle);
+
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
